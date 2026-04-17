70afcb14695dbf59af5ff53f84a4c49d6274688d
jharris49 <22jharris@wakatipu.school.nz>
2026-04-17 17:24:50 +1200

    Frame 2 Widgets Complete
    - 2nd frame widgets completed with UI for them mostly finished.
    - 0 functionality in program yet.
    - Placeholders used for data yet to be received.
    - Clipping issues as the frames themselves have yet to be set up.

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ class GetDataGUI:
             parent,
             text = "Collecting Person Data"
         )
-        self.collecting_data_label.grid(row = 0, 
+        self.collecting_data_label.grid(
+            row = 0, 
             column = 0, 
             sticky = W, 
             padx = 20, 
@@ -21,7 +22,8 @@ class GetDataGUI:
             parent,
             text = "First name:"
         )
-        self.name_label.grid(row = 1, 
+        self.name_label.grid(
+            row = 1, 
             column = 0,  
             sticky = W, 
             padx = 20,
@@ -32,7 +34,8 @@ class GetDataGUI:
             parent,
             text = "Age:"
         )
-        self.age_label.grid(row = 2, 
+        self.age_label.grid(
+            row = 2, 
             column = 0,  
             sticky = W, 
             padx = 20,
@@ -43,7 +46,8 @@ class GetDataGUI:
             parent,
             text = "Do you have a mobile phone?"
         )
-        self.phone_label.grid(row = 3, 
+        self.phone_label.grid(
+            row = 3, 
             column = 0,  
             sticky = W,
             padx = 20,
@@ -54,9 +58,11 @@ class GetDataGUI:
             parent,
             text = "Show All"
         )
-        self.show_button.grid(row = 0, 
+        self.show_button.grid(
+            row = 0, 
             column = 1, 
             sticky = W,
+            padx = 10
         )
 
         self.first_name = StringVar()
@@ -64,7 +70,8 @@ class GetDataGUI:
             parent,
             textvariable = self.first_name
         )
-        self.first_name_entry.grid(row = 1,
+        self.first_name_entry.grid(
+            row = 1,
             column = 1, 
             sticky = W,
             padx = 10
@@ -75,7 +82,8 @@ class GetDataGUI:
             parent,
             textvariable = self.age
         )
-        self.age_entry.grid(row = 2, 
+        self.age_entry.grid(
+            row = 2, 
             column = 1, 
             sticky = W,
             padx = 10
@@ -90,9 +98,11 @@ class GetDataGUI:
             value = False,
             text = "No"
         )
-        self.phone_radiobutton_false.grid(row = 3, 
+        self.phone_radiobutton_false.grid(
+            row = 3, 
             column = 1, 
-            sticky = W
+            sticky = W,
+            padx = 10
         )
 
         self.phone_radiobutton_true = Radiobutton(
@@ -101,19 +111,130 @@ class GetDataGUI:
             value = True,
             text = "Yes"
         )
-        self.phone_radiobutton_true.grid(row = 4,
+        self.phone_radiobutton_true.grid(
+            row = 4,
             column = 1, 
-            sticky = W
+            sticky = W,
+            padx = 10
         )
 
         self.enter_data_button = Button(
             parent,
             text = "Enter Data"
         )
-        self.enter_data_button.grid(row = 5, 
+        self.enter_data_button.grid(
+            row = 5, 
             column = 0,
             columnspan = 2,
             pady = 20
+        )
+
+
+        self.display_data_label = Label(
+            parent,
+            text = "Displaying Person Data"
+        )
+        self.display_data_label.grid(
+            row = 0, 
+            column = 0, 
+            sticky = W, 
+            padx = 20, 
+            pady = 20
+            )
+
+        self.f2_name_label = Label(
+            parent,
+            text = "First name:"
+        )
+        self.f2_name_label.grid(
+            row = 1, 
+            column = 0,  
+            sticky = W, 
+            padx = 20,
+            pady = 5
+        )
+
+        self.f2_age_label = Label(
+            parent,
+            text = "Age:"
+        )
+        self.f2_age_label.grid(
+            row = 2, 
+            column = 0,  
+            sticky = W, 
+            padx = 20,
+            pady = 5
+        )
+
+        self.person_phone_label = Label(
+            parent,
+            text = "(name) has a mobile phone"
+        )
+        self.person_phone_label.grid(
+            row = 3, 
+            column = 0, 
+            columnspan = 2, 
+            padx = 20,
+            pady = 5
+        )
+
+        self.previous_button = Button(
+            parent,
+            text = "Previous"
+        )
+        self.previous_button.grid(
+            row = 4, 
+            column = 0, 
+            sticky = W,
+            padx = 20,
+            pady = 10
+        )
+
+        self.add_person_button = Button(
+            parent,
+            text = "Add New Person",
+            anchor = W,
+            padx = 10
+        )
+        self.add_person_button.grid(
+            row = 0, 
+            column = 1, 
+            sticky = W,
+            padx = 10
+        )
+
+        self.person_name_label = Label(
+            parent,
+            text = "(name)"
+        )
+        self.person_name_label.grid(
+            row = 1, 
+            column = 1,  
+            sticky = W, 
+            padx = 10
+        )
+
+        self.person_age_label = Label(
+            parent,
+            text = "(age)"
+        )
+        self.person_age_label.grid(
+            row = 2, 
+            column = 1,  
+            sticky = W, 
+            padx = 10
+        )
+
+        self.next_button = Button(
+            parent,
+            text = "Next"
+        )
+        self.next_button.grid(
+            row = 4, 
+            column = 1, 
+            sticky = E,
+            padx = 20,
+            pady = 10
         )
 
 if __name__ == "__main__":
